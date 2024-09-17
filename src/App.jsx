11f5b48d4de70c919b825940197fbcc10554c3cb
Santiago Blachere <blachere.asantiago@gmail.com>
2024-09-17 17:15:56 -0300

    feat: edit and delete job experience

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,48 @@ function App() {
 
   const [jobExperience, setJobExperience] = useState([])
 
+  const handleEditJob = (key) => {
+    setJobExperience((previousJobExperience) => {
+      const jobToEdit = previousJobExperience.map((previousJob) => {
+        return previousJob.jobKey === key ? {...previousJob, editMode: true} : previousJob
+      })
+      return jobToEdit
+    })
+  }
+
+  const handleDeleteJob = (key) => {
+    setJobExperience((previousJobExperience) => {
+      const newJobExperience = previousJobExperience.filter((previousJob) => {
+        return previousJob.jobKey !== key;
+
+      })
+      return newJobExperience
+    })
+
+  }
+  const handleEditChange = (e, key, property) => {
+    const { value } = e.target;
+    setJobExperience((previousJobExperience) => {
+      return previousJobExperience.map((job) => {
+        if (job.jobKey === key) {
+          return { ...job, [property]: value };
+        }
+        return job;
+      });
+    });
+  };
+  const handleSaveChange =(key) => {
+    setJobExperience((previousJobExperience => {
+      return previousJobExperience.map((job) => {
+        if (job.jobKey === key) {
+          return { ...job, editMode: false };
+        }
+        return job;
+      })
+    }))
+  }
+
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -24,17 +66,21 @@ function App() {
     }))
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setJobExperience(prevData => [
-      ...prevData,
-      {
-        jobTitle: e.target[0].value,
-        jobDescription: e.target[1].value,
-        jobDate: e.target[2].value
-      }
-    ])
-  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const newJobExperience = {
+      jobKey: crypto.randomUUID(),
+      jobTitle: form.jobTitle.value,
+      jobDescription: form.jobDescription.value,
+      jobDate: form.dateRange.value,
+      editMode: false
+    };
+    setJobExperience((previousJobExperience) => [...previousJobExperience, newJobExperience]);
+    form.reset()
+  };
+
 
   return (
     <div className="container">
@@ -42,7 +88,7 @@ function App() {
         <Inputs jobExperience={jobExperience} handleSubmit={handleSubmit} formData={formData} handleChange={handleChange} />
       </div>
       <div className="resume-container">
-        <Resume jobExperience={jobExperience} formData={formData} />
+        <Resume handleEditChange={handleEditChange} handleSaveChange={handleSaveChange} handleDeleteJob={handleDeleteJob} handleEditJob={handleEditJob} jobExperience={jobExperience} formData={formData} />
       </div>
     </div>
   )
--- src/cvpreview/Resume.jsx
+++ src/cvpreview/Resume.jsx
@@ -1,4 +1,4 @@
-function Resume({jobExperience, formData, submited}) {
+function Resume({handleEditChange, handleSaveChange, jobExperience, formData, submited, handleEditJob, handleDeleteJob}) {
     console.log(jobExperience)
     return (
         <div className="resume">
@@ -8,14 +8,24 @@ function Resume({jobExperience, formData, submited}) {
                 <p className="aboutMe">{formData.aboutMe}</p>
                 {submited ?<h2>{formData.jobTitle}</h2> : null}
             </div>  
-            {jobExperience.length > 0 && <h2>JOB EXPERIENCE</h2>}
+            {jobExperience.length > 0&& <h2>JOB EXPERIENCE</h2>}
             {jobExperience.map( (job) => {
                 return (
-                <div>
+                <div key={job.jobKey}>
                     <h2>{job.jobTitle}</h2>
                     <p>{job.jobDescription}</p>
                     <p>{job.jobDate}</p>
-                </div>
+                    <button onClick={() => handleEditJob(job.jobKey)}>Edit</button>
+                    <button onClick={() => handleDeleteJob(job.jobKey)}>Delete</button>   
+                    {job.editMode && (
+                        <div>
+                            <input onChange={(e) => handleEditChange(e, job.jobKey, "jobTitle")} value={job.jobTitle} type="text" name="jobTitle" id="jobTitle" />
+                            <input onChange={(e) => handleEditChange(e, job.jobKey, "jobDescription")} value={job.jobDescription} type="text" name="jobDescription" id="jobDescription" />
+                            <input onChange={(e) => handleEditChange(e, job.jobKey, "jobDate")} value={job.jobDate} type="text" name="jobDate" id="jobDate" />
+                            <button onClick={() => handleSaveChange(job.jobKey)}>Save</button>
+                        </div>
+                    )}             
+                </div>              
                 )               
             })} 
         </div>
